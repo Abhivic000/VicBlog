267892e1a173a0eee3b098782abf1b722402bb9a
Abhivic <1032210906@tcetmumbai.in>
2024-02-11 18:05:23 +0530

    change1

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-mongoose.connect('mongodb+srv://abhisingh071103:i13ZHvMJDJxaHKKK@cluster0.fme5lgv.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://abhisingh071103:<password>@cluster0.fme5lgv.mongodb.net/?retryWrites=true&w=majority');
 
 app.post('/register', async (req,res) => {
   const {username,password} = req.body;
